9e7c19a4b415700353cfd31b9558151788676d9b
Farhan Basheer <farhanfb6@outlook.com>
2023-10-24 15:22:13 +0500

    GUI for Task one Done

--- Elevator_System/Form1.Designer.cs
+++ Elevator_System/Form1.Designer.cs
@@ -28,175 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnElevatorRequestToGFloor = new System.Windows.Forms.Button();
+            this.btnElevatorRequestToFFloor = new System.Windows.Forms.Button();
+            this.lblDisplayFloorNumberInsideElevator = new System.Windows.Forms.Label();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRequestToFFloor = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnRequestToGFloor = new System.Windows.Forms.Button();
+            this.btnRequestLog = new System.Windows.Forms.Button();
+            this.groupBox0 = new System.Windows.Forms.GroupBox();
+            this.lblDisplayFloorNumber = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox0.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnElevatorRequestToGFloor);
+            this.groupBox3.Controls.Add(this.btnElevatorRequestToFFloor);
+            this.groupBox3.Controls.Add(this.lblDisplayFloorNumberInsideElevator);
+            this.groupBox3.Location = new System.Drawing.Point(301, 66);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(170, 209);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Inside Elevator";
+            // 
+            // btnElevatorRequestToGFloor
+            // 
+            this.btnElevatorRequestToGFloor.Location = new System.Drawing.Point(48, 123);
+            this.btnElevatorRequestToGFloor.Name = "btnElevatorRequestToGFloor";
+            this.btnElevatorRequestToGFloor.Size = new System.Drawing.Size(75, 23);
+            this.btnElevatorRequestToGFloor.TabIndex = 5;
+            this.btnElevatorRequestToGFloor.Text = "0";
+            this.btnElevatorRequestToGFloor.UseVisualStyleBackColor = true;
+            // 
+            // btnElevatorRequestToFFloor
+            // 
+            this.btnElevatorRequestToFFloor.Location = new System.Drawing.Point(48, 74);
+            this.btnElevatorRequestToFFloor.Name = "btnElevatorRequestToFFloor";
+            this.btnElevatorRequestToFFloor.Size = new System.Drawing.Size(75, 23);
+            this.btnElevatorRequestToFFloor.TabIndex = 4;
+            this.btnElevatorRequestToFFloor.Text = "1";
+            this.btnElevatorRequestToFFloor.UseVisualStyleBackColor = true;
+            // 
+            // lblDisplayFloorNumberInsideElevator
+            // 
+            this.lblDisplayFloorNumberInsideElevator.AutoSize = true;
+            this.lblDisplayFloorNumberInsideElevator.Location = new System.Drawing.Point(64, 33);
+            this.lblDisplayFloorNumberInsideElevator.Name = "lblDisplayFloorNumberInsideElevator";
+            this.lblDisplayFloorNumberInsideElevator.Size = new System.Drawing.Size(13, 13);
+            this.lblDisplayFloorNumberInsideElevator.TabIndex = 3;
+            this.lblDisplayFloorNumberInsideElevator.Text = "--";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Controls.Add(this.btnRequestLog);
+            this.groupBox4.Location = new System.Drawing.Point(508, 69);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(163, 206);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Log Information";
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button4);
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(260, 69);
+            this.groupBox1.Controls.Add(this.btnRequestToFFloor);
+            this.groupBox1.Location = new System.Drawing.Point(113, 114);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(170, 183);
-            this.groupBox1.TabIndex = 5;
+            this.groupBox1.Size = new System.Drawing.Size(150, 80);
+            this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Inside Elevator";
+            this.groupBox1.Text = "Floor 0";
             // 
-            // button4
+            // btnRequestToFFloor
             // 
-            this.button4.Location = new System.Drawing.Point(48, 123);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "G";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnRequestToFFloor.Location = new System.Drawing.Point(38, 36);
+            this.btnRequestToFFloor.Name = "btnRequestToFFloor";
+            this.btnRequestToFFloor.Size = new System.Drawing.Size(75, 23);
+            this.btnRequestToFFloor.TabIndex = 5;
+            this.btnRequestToFFloor.Text = "1";
+            this.btnRequestToFFloor.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // groupBox2
             // 
-            this.button3.Location = new System.Drawing.Point(48, 74);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "1";
-            this.button3.UseVisualStyleBackColor = true;
+            this.groupBox2.Controls.Add(this.btnRequestToGFloor);
+            this.groupBox2.Location = new System.Drawing.Point(113, 195);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(150, 80);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Floor 1";
+            // 
+            // btnRequestToGFloor
+            // 
+            this.btnRequestToGFloor.Location = new System.Drawing.Point(38, 34);
+            this.btnRequestToGFloor.Name = "btnRequestToGFloor";
+            this.btnRequestToGFloor.Size = new System.Drawing.Size(75, 23);
+            this.btnRequestToGFloor.TabIndex = 9;
+            this.btnRequestToGFloor.Text = "0";
+            this.btnRequestToGFloor.UseVisualStyleBackColor = true;
+            // 
+            // btnRequestLog
+            // 
+            this.btnRequestLog.Location = new System.Drawing.Point(7, 20);
+            this.btnRequestLog.Name = "btnRequestLog";
+            this.btnRequestLog.Size = new System.Drawing.Size(150, 23);
+            this.btnRequestLog.TabIndex = 0;
+            this.btnRequestLog.Text = "Request Log";
+            this.btnRequestLog.UseVisualStyleBackColor = true;
+            // 
+            // groupBox0
+            // 
+            this.groupBox0.Controls.Add(this.lblDisplayFloorNumber);
+            this.groupBox0.Location = new System.Drawing.Point(113, 65);
+            this.groupBox0.Name = "groupBox0";
+            this.groupBox0.Size = new System.Drawing.Size(150, 43);
+            this.groupBox0.TabIndex = 9;
+            this.groupBox0.TabStop = false;
+            this.groupBox0.Text = "Floor Number Display";
+            // 
+            // lblDisplayFloorNumber
+            // 
+            this.lblDisplayFloorNumber.AutoSize = true;
+            this.lblDisplayFloorNumber.Location = new System.Drawing.Point(62, 16);
+            this.lblDisplayFloorNumber.Name = "lblDisplayFloorNumber";
+            this.lblDisplayFloorNumber.Size = new System.Drawing.Size(13, 13);
+            this.lblDisplayFloorNumber.TabIndex = 0;
+            this.lblDisplayFloorNumber.Text = "--";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(58, 37);
+            this.label1.Location = new System.Drawing.Point(7, 50);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 3;
+            this.label1.TabIndex = 1;
             this.label1.Text = "label1";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.button5);
-            this.groupBox2.Location = new System.Drawing.Point(467, 69);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(163, 183);
-            this.groupBox2.TabIndex = 6;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Log Information";
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.label2);
-            this.groupBox3.Controls.Add(this.button1);
-            this.groupBox3.Location = new System.Drawing.Point(72, 69);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(150, 97);
-            this.groupBox3.TabIndex = 7;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Floor 0";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(38, 32);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "label2";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(38, 51);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // groupBox4
-            // 
-            this.groupBox4.Controls.Add(this.label3);
-            this.groupBox4.Controls.Add(this.button2);
-            this.groupBox4.Location = new System.Drawing.Point(72, 192);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(150, 118);
-            this.groupBox4.TabIndex = 8;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Floor 1";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(38, 41);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "label3";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(38, 57);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "0";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(7, 20);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(150, 23);
-            this.button5.TabIndex = 0;
-            this.button5.Text = "Request Log";
-            this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.groupBox4);
-            this.Controls.Add(this.groupBox3);
+            this.ClientSize = new System.Drawing.Size(781, 362);
+            this.Controls.Add(this.groupBox0);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Window";
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox0.ResumeLayout(false);
+            this.groupBox0.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnElevatorRequestToGFloor;
+        private System.Windows.Forms.Button btnElevatorRequestToFFloor;
+        private System.Windows.Forms.Label lblDisplayFloorNumberInsideElevator;
         private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnRequestToFFloor;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnRequestToGFloor;
+        private System.Windows.Forms.Button btnRequestLog;
+        private System.Windows.Forms.GroupBox groupBox0;
+        private System.Windows.Forms.Label lblDisplayFloorNumber;
+        private System.Windows.Forms.Label label1;
     }
 }
 
